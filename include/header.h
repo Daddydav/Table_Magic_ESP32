87ca85c1dd7231bd755f54351cd0d1017d76455f
Daddydav <david.cadenas@free.fr>
2024-07-18 14:33:48 +0200

    Premier code

---
+++ include/header.h
@@ -0,0 +1,41 @@
+#define MCP23017_ADR 0x27
+#define I2C_Freq 400000
+#define I2C_SDA 14
+#define I2C_SCL 13
+
+#define RD_1 1  //PA1
+#define GN_1 2  //PA2
+#define BE_1 3  //PA3
+#define RD_2 4  //PA4
+#define GN_2 5  //PA5
+#define BE_2 6  //PA6
+#define RD_3 7  //PA7
+#define GN_3 8  //PB0
+#define BE_3 9  //PB1
+#define RD_4 10 //PB2
+#define GN_4 11 //PB3
+#define BE_4 12 //PB4
+#define RD_5 13 //PB5
+#define GN_5 14 //PB6
+#define BE_5 15 //PB7
+
+#define FEU         "74 16 C8 87" //1
+#define AIR         "74 0B 8B 87" //2
+#define EAU         "74 01 CA 87" //3
+#define TERRE       "64 F5 F8 87" //4
+#define LUMIERE     "64 EA 25 87" //5
+#define ILLUSION    "64 DE 53 87" //6
+#define FIN         "64 D2 89 87" //7
+#define CONTROLE    "64 C6 DB 87" //8
+#define DEBUT       "64 BD BA 87" //9
+#define NEGATION    "64 B2 5A 87" //10
+#define TEMPS       "74 28 64 87" //11
+#define CREATURE    "74 33 FE 87" //12
+#define PROTECTION  "74 3F 7F 87" //13
+#define OFFRANDE    "74 4A D2 87" //14
+#define ENERGIE     "74 45 24 87" //15
+#define VIE         "74 39 BA 87" //16
+#define CREATION    "74 2E 2F 87" //17
+#define PERMANENCE  "74 22 85 87" //18
+#define ESPRIT      "74 1C 7A 87" //19
+#define CHANGEMENT  "74 0B 80 87" //20
